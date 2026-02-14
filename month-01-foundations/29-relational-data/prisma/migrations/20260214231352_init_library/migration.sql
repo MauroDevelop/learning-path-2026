6ee138f7836dc75881382da1e7fda1e6ea58befc
Mauro Algañaraz <maurodevelop.git@gmail.com>
2026-02-14 20:22:42 -0300

    feat(29mod): init relational module and refactor project structure
    - REFACTOR (Module 28): Moved root 'prisma/' folder and '.env' file into '28-database-prisma/' to isolate module environments.
    - FEAT (Module 29): Initialized new Prisma setup for Library System.
    - Created 'Author' and 'Book' models with 1:N relation.
    - Configured independent database connection (library_db).
    - Applied initial migration successfully.

---
+++ month-01-foundations/29-relational-data/prisma/migrations/20260214231352_init_library/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `Author` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `bio` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `sumary` VARCHAR(191) NULL,
+    `published` BOOLEAN NOT NULL DEFAULT false,
+    `authorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Book` ADD CONSTRAINT `Book_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Author`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
