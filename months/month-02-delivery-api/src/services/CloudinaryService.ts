77fa1444e231d3fee7e320a89982b56dcdc738e0
MauroDevelop <maurodevelop.git@gmail.com>
2026-03-08 23:45:30 -0300

    feat(upload): implement Cloudinary service and Multer middleware - Month 02, week 2.

---
+++ months/month-02-delivery-api/src/services/CloudinaryService.ts
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import { AppError } from "../shared/errors/AppError";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    api_key: process.env.CLOUDINARY_API_KEY!,
+    api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export class ClaudinaryService {
+
+    static async uploadImage(fileBufer: Buffer): Promise<string> {
+        return new Promise((resolve, reject) => {
+
+            const stream = cloudinary.uploader.upload_stream(
+                { folder: 'delivery-api-products' },
+                (error, result) => {
+                    if (error) {
+                        throw reject(new AppError('Error uploading image to Cloudinary', 500))
+                    }
+                    return resolve(result?.secure_url as string)
+                }
+            );
+            stream.end(fileBufer);
+        });        
+    }
+}
